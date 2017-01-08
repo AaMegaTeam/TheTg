902b3bf183230e92a3a475cd188668ac3d568c65
AaMegaTeam <hamed.dehghani321@gmail.com>
2017-01-08 09:23:52 -0600

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -99,7 +99,7 @@ function vardump(value, depth, key)
   elseif type(value)  == 'function' or
     type(value) == 'thread' or
     type(value) == 'userdata' or
-    value == nil then --@MuteTeam
+    value == nil then --@pedaret
     print(spaces .. tostring(value))
   elseif type(value)  == 'string' then
     print(spaces .. linePrefix .. '"' .. tostring(value) .. '",')
